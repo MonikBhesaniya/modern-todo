1710022fc920e1b101ffe784c499400ac3922723
MonikBhesaniya <60084275+MonikBhesaniya@users.noreply.github.com>
2020-08-14 12:05:04 +0530

    deployment ready

--- server.js
+++ server.js
@@ -7,10 +7,6 @@ const app = express();
 
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.send("App Running");
-});
-
 app.use("/api/user", require("./routes/api/user"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/task", require("./routes/api/task"));
