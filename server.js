415d8c9546c4de6d2ef3fb4bf2bcf4272aff01da
MonikBhesaniya <60084275+MonikBhesaniya@users.noreply.github.com>
2020-08-14 10:13:30 +0530

    final commit

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 const express = require("express");
 const connectDB = require("./config/db");
-const cors = require("cors");
 
 connectDB();
 const app = express();
-
-app.use(cors());
 
 app.use(express.json({ extended: false }));
 
