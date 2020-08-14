a495593efbc9f248a596f0d77d352da267977592
MonikBhesaniya <60084275+MonikBhesaniya@users.noreply.github.com>
2020-08-14 10:46:53 +0530

    final commit

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
 
 connectDB();
 const app = express();
@@ -13,6 +14,14 @@ app.get("/", (req, res) => {
 app.use("/api/user", require("./routes/api/user"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/task", require("./routes/api/task"));
+
+if (process.env.NODE_ENV) {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
